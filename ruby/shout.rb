ba0ae77e0932f14ca4defeaea3aba6388f17123d
Jason Buss <jasonmbuss@gmail.com>
2016-09-08 10:34:54 -0500

    created a flight and shout mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,46 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + "!!!!!!" + " :D XD"
+# 	end
+
+# 	# we'll put some methods here soon, but this code is fine for now!
+# end
+
+# p Shout.yell_happily("Hi")
+# p Shout.yell_angrily("Go Away")
+
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		puts words + "!!!" + " :(\n-#{@name}"
 	end
-
-	def self.yell_happily(words)
-		words + "!!!!!!" + " :D XD"
+	def yell_happily(words)
+		puts words + "!!!!!!" + " :D XD\n-#{@name}"
 	end
-
-	# we'll put some methods here soon, but this code is fine for now!
 end
 
-p Shout.yell_happily("Hi")
-p Shout.yell_angrily("Go Away")
+class Person
+	include Shout
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+	end
+end
+
+class Knight
+	include Shout
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+	end
+end
+
+person = Person.new("John")
+person.yell_happily("Hey friend")
+
+knight = Knight.new("King Arthur")
+knight.yell_angrily("I asked for a round table")
